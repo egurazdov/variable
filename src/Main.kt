1810d2c7e2010ac982fbbd015d8d2f858984d6a4
Egurazdov <egurazdov@yandex.ru>
2024-10-14 20:07:23 +0300

    Initial commit

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,8 @@
 fun main() {
-    println("Hello World!")
+    val age: Int = 41
+    val name: String = "Алексей"
+    val isStudent: Boolean = true
+    val salary: Double = 90000.0
+
+    println("Age: " + age + " | Name: " + name + " | Is Student: " + isStudent + " | Salary: " + salary)
 }
